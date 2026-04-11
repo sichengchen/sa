34fc0760b9cc838cc3a1fe2b61ceb146df14977f
Sicheng Chen <csc0330@hotmail.com>
2026-04-10 21:44:49 -0400

    refactor(monorepo): reduce src cli and tui wrappers

--- src/connectors/tui/index.ts
+++ src/connectors/tui/index.ts
@@ -1,10 +1,3 @@
 #!/usr/bin/env bun
 
-import React from "react";
-import { render } from "ink";
-import { App } from "./App.js";
-import { createTuiClient } from "./client.js";
-
-const client = createTuiClient();
-
-render(React.createElement(App, { client }));
+import "../../../packages/connectors/src/tui/index.js";
